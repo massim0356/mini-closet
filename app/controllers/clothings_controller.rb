a7240df6da3abed10d2b1ba39a19f1e6bbccc32c
Massim Shah <massimshah@hotmail.com>
2021-06-20 22:26:07 +0900

    added actions

--- app/controllers/clothings_controller.rb
+++ app/controllers/clothings_controller.rb
@@ -1,22 +1,51 @@
 class ClothingsController < ApplicationController
+  before_action :set_clothing, only: %i[show edit update destroy]
   def index
+    @clothings = Clothing.all
   end
 
   def show
   end
 
   def new
+    @clothing = Clothing.new
   end
 
   def create
+    @clothing = Clothing.new(clothing_params)
+    @clothing.user = current_user
+    if @clothing.save
+      flash[:success] = "Clothing successfully created"
+      redirect_to @clothing
+    else
+      flash[:error] = "Something went wrong"
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @clothing.update(clothing_params)
+      redirect_to clothing_path(@clothing)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @clothing.destroy
+    redirect_to clothings_path
+  end
+
+  private
+
+  def clothing_params
+    params.require(:clothing).permit(:type, :brand, :size)
+  end
+
+  def set_clothing
+    @clothing = Clothing.find(params[:id])
   end
 end
