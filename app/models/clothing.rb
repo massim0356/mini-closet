0680afbce352bc1bab929e1895a8017d5678b364
Massim Shah <massimshah@hotmail.com>
2021-06-21 16:07:13 +0900

    added some collections for sizes and kinds

--- app/models/clothing.rb
+++ app/models/clothing.rb
@@ -1,4 +1,6 @@
 class Clothing < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+  SIZES = %w[XXS XS S M L XL XXL XXXL XXXXL]
+  KINDS = %w[T-Shirts Pants Jackets Hoodies Socks Hats Shoes]
 end
